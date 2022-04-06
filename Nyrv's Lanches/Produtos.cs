2ffa44afb67aff5ae41718f347115c1a457a1646
Nyrlivy <nyrvlivy@gmail.com>
2022-04-06 08:35:30 -0300

    ferramentas do painel

--- Nyrv's Lanches/Produtos.cs
+++ Nyrv's Lanches/Produtos.cs
@@ -6,62 +6,55 @@ using System.Threading.Tasks;
 
 namespace Nyrv_s_Lanches
 {
-     public class Produtos : IEquatable<Produtos> //Pois estou trabalhando com uma List<T>
-     {
-            public string PartNome { get; set; }
-            public int PartId { get; set; }
-            public float PartPreco { get; set; }
+    public class Produtos : IEquatable<Produtos> //Pois estou trabalhando com uma List<T>
+    {
+        public string PartNome { get; set; }
+        public int PartId { get; set; }
+        public float PartPreco { get; set; }
 
 
-            public override string ToString() //Escrever o mesmo método com parametros diferentes
-            {
-                return "|  ID: " + PartId + "  |  R$" + PartPreco + "  |  Descrição: " + PartNome; //Como é visível no sistema
-            }
-            public override bool Equals(object objeto)
-            {
-                if (objeto == null) return false;
-                Produtos objAsPart = objeto as Produtos;
-                if (objAsPart == null) return false;
-                else return Equals(objAsPart);
-            }
-            public override int GetHashCode()
-            {
-                return PartId;
-            }
-            public bool Equals(Produtos outro)
-            {
-                if (outro == null) return false;
-                return (this.PartId.Equals(outro.PartId));
-            }
-     }
-        public class ListaProduto
+        public override string ToString() //Escrever o mesmo método com parametros diferentes
         {
-            public static void Main()
-            {
-                List<Produtos> parts = new List<Produtos>(); // Lista de itens e características
-                parts.Capacity = 9999; //Limitando a quantidade de produtos para não ultrpassar o id de 4 dígitos
+            return "|  ID: " + PartId + "  |  R$" + PartPreco + "  |  Descrição: " + PartNome; //Como é visível no sistema
+        }
+        public override bool Equals(object objeto)
+        {
+            if (objeto == null) return false;
+            Produtos objAsPart = objeto as Produtos;
+            if (objAsPart == null) return false;
+            else return Equals(objAsPart);
+        }
+        public override int GetHashCode()
+        {
+            return PartId;
+        }
+        public bool Equals(Produtos outro)
+        {
+            if (outro == null) return false;
+            return (this.PartId.Equals(outro.PartId));
+        }
+    }
+    public class ListaProduto
+    {
+        public static void Main()
+        {
+            List<Produtos> parts = new List<Produtos>(); // Lista de itens e características
+            parts.Capacity = 9999; //Limitando a quantidade de produtos para não ultrpassar o id de 4 dígitos
 
-                //Nome, ID e Preço de cada combo | ID de 9999 até 8499 é para combos, após esse valor, só para produtos isolados.
-                parts.Add(new Produtos() { PartNome = "Combo Picanha    ", PartId = 8499, PartPreco = 31.99f });
-                parts.Add(new Produtos() { PartNome = "Combo Bacon      ", PartId = 9599, PartPreco = 30.99f });
-                parts.Add(new Produtos() { PartNome = "Combo Cheddar    ", PartId = 9699, PartPreco = 29.99f });
-                parts.Add(new Produtos() { PartNome = "Combo Ovo        ", PartId = 9799, PartPreco = 28.99f });
-                parts.Add(new Produtos() { PartNome = "Combo Frango     ", PartId = 9849, PartPreco = 26.99f });
-                parts.Add(new Produtos() { PartNome = "Combo Peixe      ", PartId = 9899, PartPreco = 26.99f });
-                parts.Add(new Produtos() { PartNome = "Combo Veggan     ", PartId = 9949, PartPreco = 34.99f });
+            //Nome, ID e Preço de cada combo | ID de 9999 até 8499 é para combos, após esse valor, só para produtos isolados.
+            parts.Add(new Produtos() { PartNome = "Combo Picanha    ", PartId = 8499, PartPreco = 31.99f });
+            parts.Add(new Produtos() { PartNome = "Combo Bacon      ", PartId = 9599, PartPreco = 30.99f });
+            parts.Add(new Produtos() { PartNome = "Combo Cheddar    ", PartId = 9699, PartPreco = 29.99f });
+            parts.Add(new Produtos() { PartNome = "Combo Ovo        ", PartId = 9799, PartPreco = 28.99f });
+            parts.Add(new Produtos() { PartNome = "Combo Frango     ", PartId = 9849, PartPreco = 26.99f });
+            parts.Add(new Produtos() { PartNome = "Combo Peixe      ", PartId = 9899, PartPreco = 26.99f });
+            parts.Add(new Produtos() { PartNome = "Combo Veggan     ", PartId = 9949, PartPreco = 34.99f });
 
-                Console.WriteLine("\nLotação máxima de produtos:         {0}u", parts.Capacity); //Quantidade máxima de produtos na lista e no inventário.
-                Console.WriteLine("Quantidade de produtos cadastrados: {0}", parts.Count); //Quantidade de produtos cadastrados
-                Console.WriteLine();
-                foreach (Produtos aPart in parts)
-                {
-                    Console.WriteLine(aPart);
-                }
-                Console.WriteLine();
+            Console.WriteLine();
 
-            // PAINEL DE FERRAMENTA - WHILE
+        // PAINEL DE FERRAMENTA
 
-            painel:
+        painel:
             {
                 Console.WriteLine();
                 Console.WriteLine($"\n>>>>ESSE É O PAINEL DE FERRAMENTAS!<<<<" +
@@ -79,7 +72,18 @@ namespace Nyrv_s_Lanches
                 int opcaoPainel = int.Parse(Console.ReadLine());
 
 
+                // FERRAMENTA 1
 
+                Console.WriteLine("\nLotação máxima de produtos:         {0}u", parts.Capacity); //Quantidade máxima de produtos na lista e no inventário.
+                Console.WriteLine("Quantidade de produtos cadastrados: {0}", parts.Count); //Quantidade de produtos cadastrados
+                Console.WriteLine();
+
+                // FERRAMENTA 2 - VISUALIZAR
+
+                foreach (Produtos aPart in parts)
+                {
+                    Console.WriteLine(aPart);
+                }
 
                 // FERRAMENTA 3 - BUSCA POR ID E PRODUTO
 
@@ -109,60 +113,50 @@ namespace Nyrv_s_Lanches
                             {
                                 break;
                             }
+                            else if (r4 == "n")
+                            {
+                                Console.WriteLine("Quer voltar ao Painel de Ferramentas? Digite s/n");
+                                string r9 = Console.ReadLine().ToLower();
+
+                                if (r9 == "s") goto painel;
+                                else Environment.Exit(0);
+                            }
+
                             else
                             {
-                                if (r4 == "n")
-                                {
-                                    Console.WriteLine("Quer voltar ao Painel de Ferramentas? Digite s/n");
-                                    string r9 = Console.ReadLine().ToLower();
-                                    if (r9 == "s")
-                                    {
-                                        goto painel;
-                                    }
+                                Console.WriteLine("Quer voltar ao Painel de Ferramentas? Digite s/n");
+                                goto painel;
+                            }
 
-                                    else
-                                    {
-                                        Environment.Exit(0);
-                                    }
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Quer voltar ao Painel de Ferramentas? Digite s/n");
-                                    goto painel;
-                                }
+                        }
+                    }
+                    else if (r3 == "n")
+                    {
+                        Console.WriteLine("Quer voltar ao Painel de Ferramentas? Digite s/n");
+                        string r6 = Console.ReadLine().ToLower();
+                        if (r6 == "s") goto painel;
+                        else
+                        {
+                            if (r6 == "n")
+                            {
+                                Console.WriteLine("Okay, você foi desconectado do sistema e deverá efetuar autenticação para acessar novamente! Até...");
+                                Environment.Exit(0);
                             }
                         }
                     }
                     else
                     {
-                        if (r3 == "n")
-                        {
-                            Console.WriteLine("Quer voltar ao Painel de Ferramentas? Digite s/n");
-                            string r6 = Console.ReadLine().ToLower();
-                            if (r6 == "s")
-                            {
-                                goto painel;
-                            }
-                            else
-                            {
-                                if (r6 == "n")
-                                {
-                                    Console.WriteLine("Okay, você foi desconectado do sistema e deverá efetuar autenticação para acessar novamente! Até...");
-                                    Environment.Exit(0);
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Console.Write("Por favor! Irei perguntar novamente! Pressione qualquer tecla para continuar!");
-                        }
+                        Console.Write("Por favor! Irei perguntar novamente! Pressione qualquer tecla para continuar!");
                     }
-
-                    Console.ReadKey();
                 }
+
+
             }
-            }
+
+            
         }
+    }
 }
-        
+
+
 
